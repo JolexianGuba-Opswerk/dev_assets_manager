9c08ffb7d0ab8f68a447f4ae2dbed32b3e1ce77f
Jolexian Guba <je.guba@ssed-academy.opswerks.com>
2025-08-07 19:08:53 +0800

    Added Validation and Optimizing datas

--- assets/views/__init__.py
+++ assets/views/__init__.py
@@ -1,2 +1,3 @@
-from assets.views.assets_views import asset_detail, assets_list_create
+from assets.views.assets_views import asset_detail, assets_list_create, get_users_assets
 from assets.views.assets_history_views import asset_history_list
+from assets.views.employee_views import employee_list

a877f52e3fca933032d093734b5408984f4a7031
Jolexian Guba <je.guba@ssed-academy.opswerks.com>
2025-08-12 17:39:56 +0800

    - Added JWT authentication - Views level Permission for admin and staff access - Added schema swagger-ui

--- devassets_manager/urls.py
+++ devassets_manager/urls.py
@@ -1,8 +1,19 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api/', include('assets.urls')),
+
+                  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+                  path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+                  path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+                  path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
               ] + debug_toolbar_urls()

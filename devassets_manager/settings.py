b3ea278822bf89492477fe52f7b8be431afa414e
Jolexian Guba <je.guba@ssed-academy.opswerks.com>
2025-08-13 23:19:11 +0800

    Remove sensitive keys from settings.py

--- devassets_manager/settings.py
+++ devassets_manager/settings.py
@@ -175,9 +175,10 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'devassetsmanager@gmail.com'
-EMAIL_HOST_PASSWORD = 'kckd rdgd ftdu uuvi'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+

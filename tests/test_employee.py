93828adcfd77d67dc75787789974e8083d00f944
Jolexian Guba <je.guba@ssed-academy.opswerks.com>
2025-08-20 16:57:29 +0800

    Testing pre-commit hooks

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -6,7 +6,7 @@ from rest_framework.test import APIClient, APITestCase
 from assets.models import Department, EmployeeProfile
 
 
-class employee_test(APITestCase):
+class employee_tests(APITestCase):
     def setUp(self):
         self.client = APIClient()
         # Admin User & Normal User Creation

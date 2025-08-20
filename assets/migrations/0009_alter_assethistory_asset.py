46e1196911d479aa7bdf010a1bbf9af30deeb6d1
Jolexian Guba <je.guba@ssed-academy.opswerks.com>
2025-08-20 17:50:19 +0800

    Added Pre-commit Hook
    - Running test using the container's db instead of local

--- assets/migrations/0009_alter_assethistory_asset.py
+++ assets/migrations/0009_alter_assethistory_asset.py
@@ -7,13 +7,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('assets', '0008_alter_employeeprofile_user'),
+        ("assets", "0008_alter_employeeprofile_user"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='assethistory',
-            name='asset',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='assets', to='assets.asset'),
+            model_name="assethistory",
+            name="asset",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="assets",
+                to="assets.asset",
+            ),
         ),
     ]
